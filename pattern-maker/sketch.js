a7b67e91bad192dbcc882a5bd8e5b998dacb7d24
quangnle <mnest2409@yahoo.com>
2024-05-20 17:13:51 +0700

    udpate pattern maker

--- pattern-maker/sketch.js
+++ pattern-maker/sketch.js
@@ -1,16 +1,51 @@
 function setup(){
     const thecanvas = document.getElementById("canvasArea");
-    createCanvas(400, 400, thecanvas);
+    createCanvas(500, 400, thecanvas);
     generateDefaultPattern();
+}
+
+function drawAnt(x, y, size, color){
+    push();       
+    // draw the ant
+    translate(x, y);
+    
+    // draw the head
+    fill(50);
+    ellipse(0, size * - 0.4, size * 0.5, size * 0.5);
+
+    // draw 2 dots on the head to represent eyes
+    stroke(255);        
+    point(-size * 0.1, -size * 0.6);
+    point(size * 0.1, -size * 0.6);
+
+    // draw the lower body
+    stroke(0);
+    fill(color);
+    ellipse(0, 0, size, size);
+    
+    // draw the pattern
+    stroke(0);
+    //strokeWeight(1.5);
+    for (let i = 0; i < pattern.length; i++) {
+        for (let j = 0; j < pattern.length; j++) {
+            if (pattern[j][i] === 1) {
+                fill(0);
+                point(i - size/2, j - size/2);
+                //ellipse(i - size/2, j - size/2, 5, 5);
+            }
+        }
+    }        
+    pop();
 }
 
 function draw(){
     background(255);
-    const rectSize = width / pattern[0].length;
+    // draw the pattern
+    const rectSize = height / pattern[0].length;
     for (let i = 0; i < pattern.length; i++){
         for (let j = 0; j < pattern[i].length; j++){
             if (pattern[i][j] === -1){
-                fill("#f00");
+                fill("#000");
             } else if (pattern[i][j] === 0){
                 fill(255);
             } else {
@@ -19,11 +54,14 @@ function draw(){
             rect(j * rectSize, i * rectSize, rectSize, rectSize);
         }
     }
+
+    // draw the ant
+    drawAnt(height + 10, 20, pattern[0].length, "#f00");    
 }
 
 function mousePressed(){
     if (pattern &&  pattern.length>0){
-        const rectSize = width / pattern[0].length;
+        const rectSize = height / pattern[0].length;
         const i = Math.floor(mouseY / rectSize);
         const j = Math.floor(mouseX / rectSize);
         //boundary check
