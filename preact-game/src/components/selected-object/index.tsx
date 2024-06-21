cd304a18d45a5f72084a0efe8cf1c14c641fa1b4
congminhle44 <nhokdevil123@gmail.com>
2024-06-21 16:15:23 +0700

    fix: sell bug

--- preact-game/src/components/selected-object/index.tsx
+++ preact-game/src/components/selected-object/index.tsx
@@ -1,114 +1,140 @@
-import Bug from "@/core/entity/bug"
-import { GAME_STATE, selectedObject } from "@/core/gameState"
-import { useEffect, useMemo, useRef, useState } from "react"
-import BorderContainer from "../border-container"
-import Flower from "@/core/entity/flower"
-import { MAX_POLLENS, SPAWN_DURATION } from "@/core/constants"
-import BugPattern from "../bug-pattern"
-import BringToMarket from "../bring-to-market"
-import Button from "../common/button"
-import { sellBugEffect } from "@/core/effect"
-import api from "@/core/axios"
+import Bug from "@/core/entity/bug";
+import { GAME_STATE, selectedObject } from "@/core/gameState";
+import { useEffect, useMemo, useRef, useState } from "react";
+import BorderContainer from "../border-container";
+import Flower from "@/core/entity/flower";
+import { MAX_POLLENS, SPAWN_DURATION } from "@/core/constants";
+import BugPattern from "../bug-pattern";
+import BringToMarket from "../bring-to-market";
+import Button from "../common/button";
+import { sellBugEffect } from "@/core/effect";
+import api from "@/core/axios";
 
-export default function SelectedObject () {
-  const canvasRef = useRef(null)
+export default function SelectedObject() {
+  const canvasRef = useRef(null);
 
   const [staticData, setStaticData] = useState<{
-    hungerRate?: number
-    spawningTime?: number
-    numberOfPollens?: number
-  
+    hungerRate?: number;
+    spawningTime?: number;
+    numberOfPollens?: number;
   }>({
     hungerRate: 0,
     spawningTime: 0,
     numberOfPollens: 0,
-  })
+  });
 
   const updateSelectedObject = () => {
     if (canvasRef.current && selectedObject.value) {
-      const ctx = (canvasRef.current as HTMLCanvasElement).getContext('2d')
-      if (!ctx) return
-      ctx.fillStyle = "#77dd22"
-      ctx.strokeStyle = "#000"
-      ctx.rect(0, 0, 64, 64)
-      ctx.fill()
-      ctx.stroke()
-      selectedObject.value?.drawIcon(ctx, 32, 32)
-
+      const ctx = (canvasRef.current as HTMLCanvasElement).getContext("2d");
+      if (!ctx) return;
+      ctx.fillStyle = "#77dd22";
+      ctx.strokeStyle = "#000";
+      ctx.rect(0, 0, 64, 64);
+      ctx.fill();
+      ctx.stroke();
+      selectedObject.value?.drawIcon(ctx, 32, 32);
       if (selectedObject.value instanceof Flower) {
         setStaticData({
           spawningTime: (selectedObject.value as Flower).spawningTime,
-          numberOfPollens: (selectedObject.value as Flower).numberOfPollens
-        })
+          numberOfPollens: (selectedObject.value as Flower).numberOfPollens,
+        });
       }
     }
-  }
+  };
 
   useEffect(() => {
-    updateSelectedObject()
-  }, [selectedObject.value])
+    updateSelectedObject();
+  }, [selectedObject.value?._id]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      updateSelectedObject()
-    }, 1000)
+      updateSelectedObject();
+    }, 1000);
 
     return () => {
-      clearInterval(interval)
+      clearInterval(interval);
+    };
+  }, []);
+
+  const handleSellBugApi = async (bug: Bug) => {
+    try {
+      const _find = GAME_STATE.farm.value.colony.findIndex(
+        (x) => x === selectedObject.value
+      );
+      if (_find) {
+        await api.sellBugs({
+          tankId: GAME_STATE.tank.value?._id,
+          bugIds: [bug._id],
+        });
+        GAME_STATE.farm.value.colony.splice(_find, 1);
+        sellBugEffect(bug.x, bug.y);
+        GAME_STATE.user.value!.money += 1;
+      }
+    } catch (err: any) {
+      alert(err?.response?.data.message);
     }
-  }, [])
+  };
 
   const handleSellBug = async () => {
     if (!(GAME_STATE.user.value && GAME_STATE.tank.value)) return;
     if (GAME_STATE.farm.value.colony.length === 1) return;
-    const _find = GAME_STATE.farm.value.colony.findIndex(x => x === selectedObject.value)
-    if (_find >= 0) {
-      const [_bug] = GAME_STATE.farm.value.colony.splice(_find, 1)
-      sellBugEffect(_bug.x, _bug.y)
-      GAME_STATE.user.value.money += 1
-      await api.sellBugs({
-        tankId: GAME_STATE.tank.value?._id,
-        bugIds: [_bug._id]
-      })
+    if (selectedObject.value instanceof Bug) {
+      const _bug = selectedObject.value;
+      const defaultGene = _bug?.genes?.find((gene) => gene.name === "default");
+      if (defaultGene && Math.round((defaultGene.score / total!) * 100) < 100) {
+        const confirmText = confirm("This is not a 100% default bug");
+        if (confirmText) {
+          handleSellBugApi(_bug);
+        }
+      } else {
+        handleSellBugApi(_bug);
+      }
     }
-  }
+  };
 
   const handleRemoveFlower = async () => {
     GAME_STATE.farm.value.objects.forEach(async (obj, index) => {
       if (obj === selectedObject.value) {
-        const removeFlower = GAME_STATE.farm.value.objects.splice(index, 1)
-  
-        await api.removeFlower(removeFlower[0]._id)
+        const removeFlower = GAME_STATE.farm.value.objects.splice(index, 1);
+
+        await api.removeFlower(removeFlower[0]._id);
       }
-    })
-  }
+    });
+  };
   const handleRemoveAllFlower = async () => {
-    GAME_STATE.farm.value.objects = GAME_STATE.farm.value.objects.filter((obj) => {
-      if (obj.numberOfPollens === 0) {
-        api.removeFlower(obj._id)
-        return false
+    GAME_STATE.farm.value.objects = GAME_STATE.farm.value.objects.filter(
+      (obj) => {
+        if (obj.numberOfPollens === 0) {
+          api.removeFlower(obj._id);
+          return false;
+        }
+        return true;
       }
-      return true
-    })
-  }
+    );
+  };
 
   const total = useMemo(() => {
     if (selectedObject.value instanceof Bug) {
-      return selectedObject.value.genes.reduce((acc, gene) => acc + gene.score, 0)
+      return selectedObject.value.genes.reduce(
+        (acc, gene) => acc + gene.score,
+        0
+      );
     }
-  }, [selectedObject.value])
+  }, [selectedObject.value]);
 
   return (
     <div className="flex flex-col h-full">
       <div className="flex justify-center gap-4 mb-4">
-        {selectedObject.value && <BorderContainer className="border-2 w-24 h-24">
-          <canvas
-            ref={canvasRef}
-            className="object-render-canvas w-full"
-            width={64}
-            height={64}
-          />
-        </BorderContainer>}
+        {selectedObject.value && (
+          <BorderContainer className="border-2 w-24 h-24">
+            <canvas
+              ref={canvasRef}
+              className="object-render-canvas w-full"
+              width={64}
+              height={64}
+            />
+          </BorderContainer>
+        )}
 
         {selectedObject.value instanceof Bug && (
           <BorderContainer className="border-2 w-24 h-24 bg-[red]">
@@ -128,7 +154,7 @@ export default function SelectedObject () {
             </p>
             {selectedObject.value.genes.map((gene, index) => (
               <p key={index}>
-                - {gene.name}: {Math.round(gene.score / total * 100)}%
+                - {gene.name}: {Math.round((gene.score / total!) * 100)}%
               </p>
             ))}
             <div className="mt-auto flex justify-between">
@@ -148,12 +174,16 @@ export default function SelectedObject () {
               Num of Pollens left: {staticData.numberOfPollens} / {MAX_POLLENS}
             </p>
             <div className="mt-auto flex flex-col gap-2 justify-between">
-              {selectedObject.value.numberOfPollens === 0 && <Button onClick={handleRemoveAllFlower}>Remove All withered Flower</Button>}
+              {selectedObject.value.numberOfPollens === 0 && (
+                <Button onClick={handleRemoveAllFlower}>
+                  Remove All withered Flower
+                </Button>
+              )}
               <Button onClick={handleRemoveFlower}>Remove flower</Button>
             </div>
           </>
         )}
       </div>
     </div>
-  )
+  );
 }
