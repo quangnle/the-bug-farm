05a7257c30e913fb9f60b60ea309e5d57dd59ba9
LuanVo <vtluan.nt.1999@gmail.com>
2024-06-25 15:55:12 +0700

    update: layout

--- preact-game/src/components/game-header/index.tsx
+++ preact-game/src/components/game-header/index.tsx
@@ -1,6 +1,7 @@
 import { MAX_FLOWERS } from "@/core/constants";
 import { GAME_STATE, sketchInstance } from "@/core/gameState";
 import { useEffect, useState } from "react";
+import "./style.css";
 
 export default function GameHeader() {
   const [staticData, setStaticData] = useState({
@@ -29,26 +30,24 @@ export default function GameHeader() {
   };
 
   return (
-    <div className="flex items-center justify-between bg-green-200 gap-8 p-4 px-8 pl-72">
-      {/* <div className="flex justify-between items-center gap-8"> */}
+    <div className="flex items-center justify-between gap-4 pl-72 relative">
       <img
         src="/assets/PrettyBugs.png"
-        width={300}
-        className="absolute -left-10 -top-20"
+        width={240}
+        className="absolute -left-10 -top-12"
       />
       <h1 className="h1">{GAME_STATE.user.value?.username}</h1>
       <div className="flex gap-8">
-        <div className="flex items-end font-bold">Tank: {GAME_STATE.tank.value?.name}</div>
-        <div className="font-bold flex items-end gap-2 text-xl">
-          <img src="/assets/icons/coin.png" width={32} />
-          <span>{staticData.money}</span>
+        <div className="holder money-holder">
+          <p>{staticData.money}</p>
         </div>
-        <div className="font-bold flex items-end gap-2 text-xl">
-          <img src="/assets/icons/bug.png" width={32} />
-          <span>{staticData.population} / {GAME_STATE.tank.value?.size}</span>
+
+        <div className="holder bug-holder">
+          <p>{staticData.population} / {GAME_STATE.tank.value?.size}</p>
         </div>
-        <div className="font-bold flex items-end">
-          Flowers: {staticData.flowers} / {MAX_FLOWERS}
+
+        <div className="holder flower-holder">
+          <p>{staticData.flowers} / {MAX_FLOWERS}</p>
         </div>
       </div>
     </div>
--- preact-game/src/pages/Home/index.tsx
+++ preact-game/src/pages/Home/index.tsx
@@ -3,12 +3,12 @@ import BorderContainer from "../../components/border-container"
 import "./style.css"
 import GameBody from "@/components/game-body"
 import FlowerPlant from "@/components/flower-plant"
-import GameHeader from "@/components/game-header"
-import SelectedObject from "@/components/selected-object"
 import { useEffect, useState } from "react"
 import { handleError } from "@/utils/helpers"
 import api from "@/core/axios"
 import { GAME_STATE } from "@/core/gameState"
+import GameHeader from "@/components/game-header"
+import SelectedObject from "@/components/selected-object"
 
 export default function Home() {
   const [loading, setLoading] = useState(true)
@@ -35,9 +35,7 @@ export default function Home() {
       <div className="mx-auto">
         <div className="flex flex-col gap-8">
           {!loading && <LoginForm />}
-          <BorderContainer className="header">
-            <GameHeader />
-          </BorderContainer>
+          <GameHeader />
           <div className="flex gap-8">
             <GameBody loading={loading} />
             <div className="flex flex-col gap-8 flex-1 min-w-[300px]">
